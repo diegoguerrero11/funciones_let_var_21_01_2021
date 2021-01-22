70abb02b6747ee21716f3659e8b165788e96314a
diego manuel guerrero torres <diegomgt11@gmail.com>
2021-01-21 22:03:05 -0500

    clase21/01/21

---
+++ main.js
@@ -0,0 +1,19 @@
+//Crear tres funciones
+
+function nombre(dato) {
+    console.log(dato);
+}
+function saludar(p1) {
+    console.log(p1);
+}
+function finalizar(){
+    console.log("nos vemos")
+}
+function despedir() {
+    console.log("nos vemos");
+}
+let dato = prompt("Como te llamas?:");
+nombre(dato);
+saludar("Hola, estas bien?")
+finalizar();
+despedir()
